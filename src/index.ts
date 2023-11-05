0b0a5c7dac4b23ee7b33688776e04ccc3f3c411e
fikrihaikal20 <disntro@gmail.com>
2023-11-05 20:30:04 +0700

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,10 @@ app.use(express.json());
 
 app.use(allRoutes)
 
-
+app.get('/', (req, res) => {
+    const nodeEnv = process.env.NODE_ENV;
+    res.send(`API ready in ${nodeEnv} environment`);
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
