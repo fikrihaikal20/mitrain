835af494dec97c73ef29542f106725fa6bcc6d4e
fikrihaikal20 <disntro@gmail.com>
2023-11-06 02:30:36 +0700

    test CI

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use(allRoutes)
 
 app.get('/', (req, res) => {
     const nodeEnv = process.env.NODE_ENV;
-    res.send(`API ready in ${nodeEnv} environment boss`);
+    res.send(`API ready in ${nodeEnv} environment bossku`);
 });
 
 const PORT = process.env.PORT;
